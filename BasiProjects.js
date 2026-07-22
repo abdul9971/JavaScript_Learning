bf6ebe434d4167826bf5fed7e1ccaebdbab3dbe6
ABDULRAHIM SHAIKH <203560115+abdul9971@users.noreply.github.com>
2026-07-22 16:16:30 +0530

    Refactor email masking and add song album management

--- BasiProjects.js
+++ BasiProjects.js
@@ -1,4 +1,4 @@
-// The Program to Hide the Email
+// ==================== The Program to Hide the Email  =============================
 function maskEmail(email){
   const atIdx = email.indexOf("@");
   const chgAst = email.slice(1,atIdx-1);
@@ -8,7 +8,7 @@ function maskEmail(email){
 const email = "apple.pie@example.com";
 console.log(maskEmail(email))  // output: a*******e@example.com
 
-// The Program to manage menu for lunch
+// ==================== The Program to manage menu for lunch =====================
 const lunches = [];   // list of lunches
 
 // function to add lunch at Ending
@@ -74,3 +74,40 @@ console.log(addLunchToStart(["Burger", "Sushi"], "Pizza"));
 
 showLunchMenu(["Greens", "Corns", "Beans"]);
 showLunchMenu(["Pizza", "Burger", "Fries", "Salad"])
+
+
+// ======================= The program to Manage Song Album =====================
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Main function to add any property and its value
+function updateRecords(records, id, prop, value){
+  if(value.trim() == ''){
+    delete records[id][prop]
+  }else if ((prop != 'tracks')&&(value.trim() != '')){
+    records[id][prop] = value;
+  }else if ((prop == 'tracks')&&(value.trim() != '')&&!('tracks' in records[id])){
+    records[id][prop] = [value];
+  }else if ((prop == 'tracks')&&(value.trim() != '')){
+    records[id][prop].push(value);
+  }
+  return records
+};
+
